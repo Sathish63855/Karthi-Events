98ec3807272fce69334502aa9ef8665e711155be
Yuvaraj007raji <yuvarajkumar517@outlook.com>
2025-10-24 00:49:41 +0530

    Added new comment in index.js for sample

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ root.render(
 );
 
 // Hi Yuvaraj
+// Hi Sathish
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
